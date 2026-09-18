c1db0f0a051f9cb378f69e777ed7d2dccae93d76
nanzindlovu18-sudo <nanzindlovu18@gmail.com>
2026-09-18 23:25:52 +0200

    Add files via upload

---
+++ login.java
@@ -0,0 +1,93 @@
+public class login {
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+
+    private boolean loginStatus;
+
+    public Login(String firstName, String lastName, String username,
+                 String password, String cellPhoneNumber) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+
+    public boolean checkUserName() {
+        return username.contains("_") && username.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity() {
+
+        if (password.length() < 8) {
+            return false;
+        }
+
+        boolean hasCapital = false;
+        boolean hasNumber = false;
+        boolean hasSpecial = false;
+
+        for (int i = 0; i < password.length(); i++) {
+
+            char character = password.charAt(i);
+
+            if (Character.isUpperCase(character)) {
+                hasCapital = true;
+            }
+
+            if (Character.isDigit(character)) {
+                hasNumber = true;
+            }
+
+            if (!Character.isLetterOrDigit(character)) {
+                hasSpecial = true;
+            }
+        }
+
+        return hasCapital && hasNumber && hasSpecial;
+    }
+
+    public boolean checkCellPhoneNumber() {
+        return cellPhoneNumber.matches("^\\+27\\d{9}$");
+    }
+
+    public String registerUser() {
+
+        if (!checkUserName()) {
+            return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+        }
+
+        if (!checkPasswordComplexity()) {
+            return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+        }
+
+        if (!checkCellPhoneNumber()) {
+            return "Cell number is incorrectly formatted or does not contain an international code; please correct the number and try again.";
+        }
+
+        return "Username successfully captured.";
+    }
+
+    public boolean loginUser(String enteredUsername, String enteredPassword) {
+
+        loginStatus = enteredUsername.equals(username)
+                && enteredPassword.equals(password);
+
+        return loginStatus;
+    }
+
+    public String returnLoginStatus() {
+
+        if (loginStatus) {
+            return "Welcome " + firstName + " " + lastName
+                    + ", it is great to see you again.";
+        }
+
+        return "Username or password incorrect, please try again.";
+    }
+}
---
+++ logintest.java
@@ -0,0 +1,110 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class loginTest {
+
+    @Test
+    public void testLoginSuccessful() {
+        Login user = new Login(
+            "Kyle",
+            "Smith",
+            "KY1_1",
+            "Ch&&sec@ke99!",
+            "+27838968976"
+        );
+
+        assertTrue(user.loginUser("KY1_1", "Ch&&sec@ke99!"));
+    }
+
+    @Test
+    public void testLoginFailed() {
+        Login user = new Login(
+            "Kyle",
+            "Smith",
+            "KY1_1",
+            "Ch&&sec@ke99!",
+            "+27838968976"
+        );
+
+        assertFalse(user.loginUser("WRONG", "wrongpassword"));
+    }
+
+    @Test
+    public void testUsernameCorrect() {
+        Login user = new Login(
+            "Kyle",
+            "Smith",
+            "KY1_1",
+            "Ch&&sec@ke99!",
+            "+27838968976"
+        );
+
+        assertTrue(user.checkUserName());
+    }
+
+    @Test
+    public void testUsernameIncorrect() {
+        Login user = new Login(
+            "Kyle",
+            "Smith",
+            "KY123456",
+            "Ch&&sec@ke99!",
+            "+27838968976"
+        );
+
+        assertFalse(user.checkUserName());
+    }
+
+    @Test
+    public void testPasswordCorrect() {
+        Login user = new Login(
+            "Kyle",
+            "Smith",
+            "KY1_1",
+            "Ch&&sec@ke99!",
+            "+27838968976"
+        );
+
+        assertTrue(user.checkPasswordComplexity());
+    }
+
+    @Test
+    public void testPasswordIncorrect() {
+        Login user = new Login(
+            "Kyle",
+            "Smith",
+            "KY1_1",
+            "password",
+            "+27838968976"
+        );
+
+        assertFalse(user.checkPasswordComplexity());
+    }
+
+    @Test
+    public void testCellPhoneCorrect() {
+        Login user = new Login(
+            "Kyle",
+            "Smith",
+            "KY1_1",
+            "Ch&&sec@ke99!",
+            "+27838968976"
+        );
+
+        assertTrue(user.checkCellPhoneNumber());
+    }
+
+    @Test
+    public void testCellPhoneIncorrect() {
+        Login user = new Login(
+            "Kyle",
+            "Smith",
+            "KY1_1",
+            "Ch&&sec@ke99!",
+            "08966553"
+        );
+
+        assertFalse(user.checkCellPhoneNumber());
+    }
+}
